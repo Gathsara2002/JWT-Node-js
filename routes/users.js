62c5532f0b0c8af4374eaa0073f3b3a170f797a6
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-01 12:12:09 +0530

    create secret key

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let jwt = require('jsonwebtoken');
+let env = require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
