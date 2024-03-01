9a202084989e6eafe4ec6b11908278f373baf1a1
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-01 18:14:50 +0530

    crate auth.js middlewear for authorization

---
+++ auth/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function (req, res, next) {
+
+    //check request has authorization  header and its include token
+    if (req.headers.authorization && req.headers.authorization.startsWith('bearer')) {
+
+        //get token
+        const token = req.headers.authorization.split(' ')[1];
+        if (token == null) {
+            res.sendStatus(401)
+        }
+
+        //verify token
+        jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+            if (err) {
+                res.sendStatus(403)
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401)
+    }
+}
