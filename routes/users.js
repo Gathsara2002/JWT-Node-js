1445ed9f6ff8b9ed2668613a46c27943f5c0a091
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-01 19:02:36 +0530

    create refresh token

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,9 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
+//store refresh token
+let refreshToken = [];
+
 router.post('/login', (req, res, next) => {
 
     //get data from req
@@ -15,8 +18,11 @@ router.post('/login', (req, res, next) => {
     //create payload
     const user = {name: username};
     //create token
-    const token = jwt.sign(user, process.env.SECRET_KEY,{expiresIn: `20s`});
-    res.send({token});
+    const accessToken = jwt.sign(user, process.env.SECRET_KEY, {expiresIn: `20s`});
+    //create refresh token
+    const refreshToken = jwt.sign(user, process.env.REFRESH_SECRET_KEY, {expiresIn: `24h`});
+    refreshToken.push(refreshToken);
+    res.send({accessToken, refreshToken});
 
 });
 
